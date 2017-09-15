f9b34f9841a786666ab677676d7c91f34439ea80
test <422538722@qq.com>
2017-09-15 15:43:45 +0800

    21.5
    MBProgressHUD基本使用

---
+++ 21.5.2 MBProgressHUD基本使用/加载进度动画MBProgressHUD/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  加载进度动画MBProgressHUD
+//
+//  Created by 李维佳 on 16/12/14.
+//  Copyright © 2016年 liz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 21.5.2 MBProgressHUD基本使用/加载进度动画MBProgressHUD/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  加载进度动画MBProgressHUD
+//
+//  Created by 李维佳 on 16/12/14.
+//  Copyright © 2016年 liz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
